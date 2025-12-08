dae8cf63be5ad569dd310fe35de3374a7878dcd4
Slubega <151421982+Slubega@users.noreply.github.com>
2025-12-08 17:48:12 -0500

    Update firebase.js

--- server/firebase.js
+++ server/firebase.js
@@ -1,7 +1,7 @@
 // server/firebase.js
 const admin = require("firebase-admin");
 
-// IMPORTANT: you need to download a service account key JSON
+// IMPORTANT: download a service account key JSON
 // from the Firebase console and save it as server/serviceAccount.json
 const serviceAccount = require("./serviceAccount.json");
 
